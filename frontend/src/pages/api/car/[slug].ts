5952184c3715d08475ba32cb1ef2b1c9e9e2f4d7
Samuel Silva <samuelsilvawb@gmail.com>
2021-03-24 20:37:51 +0000

    feat(frontend): Add BFF find cars by slug endpoint

---
+++ frontend/src/pages/api/car/[slug].ts
@@ -0,0 +1,21 @@
+import nc from "next-connect";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = nc<NextApiRequest, NextApiResponse>();
+
+handler.get((req, res) => {
+  const { slug } = req.query;
+
+  return fetch(`${process.env.API_URL}/cars?slug=${slug}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      res.status(500).json({ statusCode: 500, message: err.message });
+    });
+});
+
+export default handler;
