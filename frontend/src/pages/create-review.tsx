dbeeeeb7858a1f1b7db3dc642945ab13294ed700
Samuel Silva <samuelsilvawb@gmail.com>
2021-03-31 07:33:05 +0100

    feat(rontend): Add form error handling on create review

--- frontend/src/pages/create-review.tsx
+++ frontend/src/pages/create-review.tsx
@@ -3,8 +3,20 @@ import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-
 import { createStyles, makeStyles } from "@material-ui/core";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+type ReviewForm = {
+  review: string;
+  author: string;
+};
+
+const flexColumContainerStyles = {
+  display: "flex",
+  flexDirection: "column" as const,
+};
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -29,10 +41,13 @@ const useStyles = makeStyles((theme) =>
     label: {
       marginBottom: "5px",
     },
-    textarea: {
+    reviewWrapper: {
+      ...flexColumContainerStyles,
+      width: "100%",
       marginBottom: "30px",
     },
-    input: {
+    authorWrapper: {
+      ...flexColumContainerStyles,
       marginBottom: "30px",
     },
     button: {
@@ -41,8 +56,20 @@ const useStyles = makeStyles((theme) =>
   })
 );
 
+const schema = yup.object().shape({
+  review: yup.string().min(10).required(),
+  author: yup.string().email().required(),
+});
+
 function CreateReview(): JSX.Element {
   const styles = useStyles();
+  const { register, errors, handleSubmit } = useForm<ReviewForm>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (values: ReviewForm): void => {
+    console.log(values);
+  };
 
   return (
     <article className={styles.wrapper}>
@@ -52,22 +79,42 @@ function CreateReview(): JSX.Element {
       <Typography variant="body1" component="p" className={styles.subtitle}>
         Your review will be used to help other users to find the right car.
       </Typography>
-      <form className={styles.form}>
-        <InputLabel htmlFor="review" className={styles.label}>
-          Your Review
-        </InputLabel>
-        <Input
-          id="review"
-          rowsMin={10}
-          className={styles.textarea}
-          multiline
-          rows={7}
-        />
-        <InputLabel htmlFor="author" className={styles.label}>
-          Author Email
-        </InputLabel>
-        <Input id="author" type="email" className={styles.input} />
-        <Button variant="outlined" color="primary" className={styles.button}>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.reviewWrapper}>
+          <InputLabel htmlFor="review" className={styles.label}>
+            Your Review
+          </InputLabel>
+          <Input
+            id="review"
+            name="review"
+            rowsMin={10}
+            multiline
+            rows={7}
+            inputRef={register}
+          />
+          {errors.review && (
+            <Typography color="error" variant="body1" component="p">
+              {errors.review.message}
+            </Typography>
+          )}
+        </div>
+        <div className={styles.authorWrapper}>
+          <InputLabel htmlFor="author" className={styles.label}>
+            Author Email
+          </InputLabel>
+          <Input id="author" name="author" type="email" inputRef={register} />
+          {errors.author && (
+            <Typography color="error" variant="body1" component="p">
+              {errors.author.message}
+            </Typography>
+          )}
+        </div>
+        <Button
+          type="submit"
+          variant="outlined"
+          color="primary"
+          className={styles.button}
+        >
           Save
         </Button>
       </form>
